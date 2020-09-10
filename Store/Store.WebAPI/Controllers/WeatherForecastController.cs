40c90e82f5c52dfc631cc8c199d963f05b0bcc47
penicdijana <penic.dijana@gmail.com>
2020-09-10 22:31:20 +0200

    Createt empty projects for repository, service layers and Web API.

---
+++ Store/Store.WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Store.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+    }
+}
