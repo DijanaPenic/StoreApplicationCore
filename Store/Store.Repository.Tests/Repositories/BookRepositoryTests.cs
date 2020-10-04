3f130598bf36c04a476f2dc7b0c43c86d1013442
penicdijana <penic.dijana@gmail.com>
2020-10-04 22:49:50 +0200

    Created unit tests for book and bookstore repositories.

---
+++ Store/Store.Repository.Tests/Repositories/BookRepositoryTests.cs
@@ -0,0 +1,181 @@
+﻿using Xunit;
+using System;
+using X.PagedList;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+using Microsoft.EntityFrameworkCore;
+
+using Store.Models;
+using Store.Model.Common.Models;
+using Store.Entities;
+using Store.DAL.Context;
+using Store.Repositories;
+using Store.Repository.Mapper;
+using Store.Common.Enums;
+using Store.Common.Helpers;
+
+namespace Store.Repository.Tests
+{
+    public class BookRepositoryTests
+    {
+        private readonly IList<BookEntity> _books;
+        private readonly IMapper _mapper;
+        private readonly DbContextOptions<StoreDbContext> _dbContextOptions;
+
+        public BookRepositoryTests()
+        {
+            // Intialize books
+            _books = InitializeBooks();
+
+            // Setup auto mapper
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperRepositoryProfile>();
+                cfg.AddExpressionMapping();
+            });
+            _mapper = mapperConfiguration.CreateMapper();
+
+            // Setup database context - different DbContext instances are used to seed the database and run the tests. 
+            // This ensures that the test is not using (or tripping over) entities tracked by the context when seeding. 
+            // It also better matches what happens in web apps and services. 
+            // Source: https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/testing-sample
+            _dbContextOptions = new DbContextOptionsBuilder<StoreDbContext>().UseInMemoryDatabase(databaseName: "UnitTestsStoreDatabase").Options;
+
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Books.AddRange(_books);
+            dbContext.SaveChanges();
+        }
+
+        [Fact]
+        [Trait("Category", "GET Book")]
+        public async Task BookGetByIdAsync()
+        {
+            BookEntity book = _books.First();
+
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            IBook result = await _repository.FindByIdAsync(book.Id);
+
+            Assert.Equal(result.Id, book.Id);
+        }
+
+        [Fact]
+        [Trait("Category", "GET Books")]
+        public async Task BookGetAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            IEnumerable<IBook> result = await _repository.GetAsync();
+
+            Assert.NotNull(result);
+            Assert.Single(result);
+            Assert.Equal(_books.Count, result.Count());
+        }
+
+        [Fact]
+        [Trait("Category", "GET Books")]
+        public async Task BookGetPagedFilteredAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            string searchString = "Name";
+            Expression<Func<IBook, bool>> filterExpression = b => b.Name.Contains(searchString) || b.Author.Contains(searchString) || b.Bookstore.Name.Contains(searchString);
+            
+            IPagedList<IBook> result = await _repository.FindAsync(filterExpression, nameof(IBook.Name), true, 1, 3);
+
+            Assert.NotNull(result);
+            Assert.Equal(1, result.TotalItemCount);
+        }
+
+        [Fact]
+        [Trait("Category", "GET Books")]
+        public async Task BookGetCollectionFilteredAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            string searchString = "Name";
+            Expression<Func<IBook, bool>> filterExpression = b => b.Name.Contains(searchString) || b.Author.Contains(searchString) || b.Bookstore.Name.Contains(searchString);
+
+            IEnumerable<IBook> result = await _repository.FindAsync(filterExpression, nameof(IBook.Name), true);
+
+            Assert.NotNull(result);
+            Assert.Single(result);
+        }
+
+        [Fact]
+        [Trait("Category", "UPDATE Book")]
+        public async Task BookUpdateAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.UpdateAsync(new Book { Id = _books.First().Id, Name = "Name - updated", Author = "Author - updated" });
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        [Fact]
+        [Trait("Category", "UPDATE Book")]
+        public async Task BookUpdateByIdAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.UpdateAsync(_books.First().Id, new Book { Name = "Name - updated", Author = "Author - updated" });
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        [Fact]
+        [Trait("Category", "INSERT Book")]
+        public async Task BookInsertAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.AddAsync(new Book { Name = "Name - created", Author = "Author - created" });
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        [Fact]
+        [Trait("Category", "DELETE Book")]
+        public async Task BookDeleteAsync()
+        {
+            using StoreDbContext dbContextDelete = new StoreDbContext(_dbContextOptions);
+            BookRepository _repository = new BookRepository(dbContextDelete, _mapper);
+
+            ResponseStatus status = await _repository.DeleteByIdAsync(_books.First().Id);
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        private IList<BookEntity> InitializeBooks()
+        {
+            return new List<BookEntity>()
+            {
+                new BookEntity
+                {
+                    Id = GuidHelper.NewSequentialGuid(),
+                    Name = "Name",
+                    Author = "Author",
+                    Bookstore = new BookstoreEntity()
+                    {
+                        Id = GuidHelper.NewSequentialGuid(),
+                        Name = "Name",
+                        Location = "Location"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Store/Store.Repository.Tests/Repositories/BookstoreRepositoryTests.cs
@@ -0,0 +1,175 @@
+﻿using Xunit;
+using System;
+using X.PagedList;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+using Microsoft.EntityFrameworkCore;
+
+using Store.Models;
+using Store.Model.Common.Models;
+using Store.Entities;
+using Store.DAL.Context;
+using Store.Repositories;
+using Store.Repository.Mapper;
+using Store.Common.Enums;
+using Store.Common.Helpers;
+
+namespace Store.Repository.Tests
+{
+    public class BookstoreRepositoryTests
+    {
+        private readonly IList<BookstoreEntity> _bookstores;
+        private readonly IMapper _mapper;
+        private readonly DbContextOptions<StoreDbContext> _dbContextOptions;
+
+        public BookstoreRepositoryTests()
+        {
+            // Intialize books
+            _bookstores = InitializeBookstores();
+
+            // Setup auto mapper
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperRepositoryProfile>();
+                cfg.AddExpressionMapping();
+            });
+            _mapper = mapperConfiguration.CreateMapper();
+
+            // Setup database context - different DbContext instances are used to seed the database and run the tests. 
+            // This ensures that the test is not using (or tripping over) entities tracked by the context when seeding. 
+            // It also better matches what happens in web apps and services. 
+            // Source: https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/testing-sample
+            _dbContextOptions = new DbContextOptionsBuilder<StoreDbContext>().UseInMemoryDatabase(databaseName: "UnitTestsStoreDatabase").Options;
+
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Bookstores.AddRange(_bookstores);
+            dbContext.SaveChanges();
+        }
+
+        [Fact]
+        [Trait("Category", "GET Bookstore")]
+        public async Task BookstoreGetByIdAsync()
+        {
+            BookstoreEntity bookstore = _bookstores.First();
+
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            IBookstore result = await _repository.FindByIdAsync(bookstore.Id);
+
+            Assert.Equal(result.Id, bookstore.Id);
+        }
+
+        [Fact]
+        [Trait("Category", "GET Bookstores")]
+        public async Task BookstoreGetAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            IEnumerable<IBookstore> result = await _repository.GetAsync();
+
+            Assert.NotNull(result);
+            Assert.Single(result);
+            Assert.Equal(_bookstores.Count, result.Count());
+        }
+
+        [Fact]
+        [Trait("Category", "GET Bookstores")]
+        public async Task BookstoreGetPagedFilteredAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            string searchString = "Name";
+            Expression<Func<IBookstore, bool>> filterExpression = b => b.Name.Contains(searchString) || b.Location.Contains(searchString);
+
+            IPagedList<IBookstore> result = await _repository.FindAsync(filterExpression, nameof(IBookstore.Name), true, 1, 3);
+
+            Assert.NotNull(result);
+            Assert.Equal(1, result.TotalItemCount);
+        }
+
+        [Fact]
+        [Trait("Category", "GET Bookstores")]
+        public async Task BookstoreGetCollectionFilteredAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            string searchString = "Name";
+            Expression<Func<IBookstore, bool>> filterExpression = b => b.Name.Contains(searchString) || b.Location.Contains(searchString);
+
+            IEnumerable<IBookstore> result = await _repository.FindAsync(filterExpression, nameof(IBookstore.Name), true);
+
+            Assert.NotNull(result);
+            Assert.Single(result);
+        }
+
+        [Fact]
+        [Trait("Category", "UPDATE Bookstore")]
+        public async Task BookstoreUpdateAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.UpdateAsync(new Bookstore { Id = _bookstores.First().Id, Name = "Name - updated", Location = "Location - updated" });
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        [Fact]
+        [Trait("Category", "UPDATE Bookstore")]
+        public async Task BookstoreUpdateByIdAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.UpdateAsync(_bookstores.First().Id, new Bookstore { Name = "Name - updated", Location = "Location - updated" });
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        [Fact]
+        [Trait("Category", "INSERT Bookstore")]
+        public async Task BookstoreInsertAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.AddAsync(new Bookstore { Name = "Name - created", Location = "Location - created" });
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        [Fact]
+        [Trait("Category", "DELETE Bookstore")]
+        public async Task BookstoreDeleteAsync()
+        {
+            using StoreDbContext dbContext = new StoreDbContext(_dbContextOptions);
+            BookstoreRepository _repository = new BookstoreRepository(dbContext, _mapper);
+
+            ResponseStatus status = await _repository.DeleteByIdAsync(_bookstores.First().Id);
+
+            Assert.Equal(ResponseStatus.Success, status);
+        }
+
+        private IList<BookstoreEntity> InitializeBookstores()
+        {
+            return new List<BookstoreEntity>()
+            {
+                new BookstoreEntity
+                {
+                    Id = GuidHelper.NewSequentialGuid(),
+                    Name = "Name",
+                    Location = "Location"
+                }
+            };
+        }
+    }
+}
