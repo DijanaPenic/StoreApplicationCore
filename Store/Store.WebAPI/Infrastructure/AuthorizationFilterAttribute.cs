4fd47ff359a65d58207b817b8aaf66827df3a21d
penicdijana <penic.dijana@gmail.com>
2020-10-27 23:14:53 +0100

    Created custom Authorization attribute so that a list of roles can be easily initialized.

---
+++ Store/Store.WebAPI/Infrastructure/AuthorizationFilterAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Store.WebAPI.Infrastructure
+{
+    public class AuthorizationFilterAttribute : AuthorizeAttribute
+    {
+        public AuthorizationFilterAttribute(params string[] roles) : base()
+        {
+            Roles = string.Join(",", roles);
+        }
+    }
+}
