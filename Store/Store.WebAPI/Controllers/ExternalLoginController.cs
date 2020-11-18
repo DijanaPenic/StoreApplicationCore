7a571f457619b1653ed0acb515f3ae52e50d22c3
penicdijana <penic.dijana@gmail.com>
2020-11-18 15:43:59 +0100

    External login controller - added comments (information about the external login process).

--- Store/Store.WebAPI/Controllers/ExternalLoginController.cs
+++ Store/Store.WebAPI/Controllers/ExternalLoginController.cs
@@ -19,7 +19,25 @@ using Store.Model.Common.Models.Identity;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 // Blog post: https://chsakell.com/2019/07/28/asp-net-core-identity-series-external-provider-authentication-registration-strategy/
-// TODO - add information about authentication rules
+
+// Rule #1: Users can sign in only if their email is confirmed.
+// Rule #2: Users authenticated by an external provider are considered trusted.
+// Rule #3: Users authenticated by an external provider that select the option to associate the email address used in the provider with an existing account registered with a different email address, will have to confirm the external provider association through the existing account’s email address.
+// Rule #4: An association can only happen with already confirmed accounts.
+// Rule #5: Users authenticated by an external provider but have an existing account with the same email address that hasn’t been confirmed, have to confirm the association which eventually will automatically confirm the existing account as well. 
+
+// Authentication:
+//
+// External login already exists - authenticate
+// External login doesn't exist:
+//          The same email address is found:
+//              * email is confirmed - add external login and authenticate
+//              * email is not confirmed - send token to the email
+//          The same email address is not found - user can choose to register a new account or to associate external login with the existing account (different email)
+//              * register a new account - the system will create a new user and authenticate
+//              * associate with the existing account:
+//                  * email is confirmed - send token to the email
+//                  * email is not confirmed - return an error
 namespace Store.WebAPI.Controllers
 {
     [ApiController]
