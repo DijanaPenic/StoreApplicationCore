b07c4d73c5c1d5eb75b86abc518ea8e9beef3094
penicdijana <penic.dijana@gmail.com>
2020-11-21 00:26:32 +0100

    Fixed user mapping - ignored role property because role updates are done separately.

--- Store/Store.WebAPI/Mapper/Profiles/AutoMapperWebApiProfile.cs
+++ Store/Store.WebAPI/Mapper/Profiles/AutoMapperWebApiProfile.cs
@@ -50,8 +50,8 @@ namespace Store.WebAPI.Mapper.Profiles
             CreateMap<UserGetApiModel, IUser>();
             CreateMap<IUser, UserGetApiModel>().ForMember(dst => dst.Roles, opt => opt.MapFrom(src => src.Roles.Select(r => r.Name).ToArray()));
 
-            CreateMap<UserCreatePostApiModel, IUser>().ConstructUsing(src => new User()).ReverseMap();
-            CreateMap<UserRegisterPostApiModel, IUser>().ConstructUsing(src => new User()).ReverseMap();
+            CreateMap<UserCreatePostApiModel, IUser>().ForMember(dst => dst.Roles, opt => opt.Ignore());
+            CreateMap<UserRegisterPostApiModel, IUser>().ForMember(dst => dst.Roles, opt => opt.Ignore());
 
             CreateMap<RoleGetApiModel, IRole>().ConstructUsing(src => new Role()).ReverseMap();
 
