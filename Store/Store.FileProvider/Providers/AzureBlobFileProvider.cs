476956d43797c09402ae13bb186f3d37d46c79dc
Dijana Penić <penic.dijana@gmail.com>
2020-12-22 23:42:06 +0100

    Updated GetBlockBlobAsync method in the AzureBlobFileProvider class - storage container will be created in case it doesn't exist.

--- Store/Store.FileProvider/Providers/AzureBlobFileProvider.cs
+++ Store/Store.FileProvider/Providers/AzureBlobFileProvider.cs
@@ -19,17 +19,21 @@ namespace Store.FileProvider.Providers
 
         public async Task<bool> DeleteFileAsync(string storageName, string filePath)
         {
-            return GetBlockBlob(storageName, filePath).DeleteIfExistsAsync();
+            CloudBlockBlob blob = await GetBlockBlobAsync(storageName, filePath);
+
+            return await blob.DeleteIfExistsAsync();
         }
 
-        public Task<bool> FileExistsAsync(string storageName, string filePath)
+        public async Task<bool> FileExistsAsync(string storageName, string filePath)
         {
-            return GetBlockBlob(storageName, filePath).ExistsAsync();
+            CloudBlockBlob blob = await GetBlockBlobAsync(storageName, filePath);
+
+            return await blob.ExistsAsync();
         }
 
         public async Task<Stream> GetFileAsync(string storageName, string filePath)
         {
-            CloudBlockBlob blob = GetBlockBlob(storageName, filePath);
+            CloudBlockBlob blob = await GetBlockBlobAsync(storageName, filePath);
 
             MemoryStream memory = new MemoryStream();
             await blob.DownloadToStreamAsync(memory).ConfigureAwait(false);
@@ -40,7 +44,7 @@ namespace Store.FileProvider.Providers
 
         public async Task<string> GetFileUrlAsync(string storageName, string filePath)
         {
-            CloudBlockBlob blob = GetBlockBlob(storageName, filePath);
+            CloudBlockBlob blob = await GetBlockBlobAsync(storageName, filePath);
             string url = null;
 
             if (await blob.ExistsAsync().ConfigureAwait(false))
@@ -51,14 +55,18 @@ namespace Store.FileProvider.Providers
             return url;
         }
 
-        public Task SaveFileAsync(string storageName, string filePath, Stream fileStream)
+        public async Task SaveFileAsync(string storageName, string filePath, Stream fileStream)
         {
-            return GetBlockBlob(storageName, filePath).UploadFromStreamAsync(fileStream);
+            CloudBlockBlob blob = await GetBlockBlobAsync(storageName, filePath);
+
+            await blob.UploadFromStreamAsync(fileStream);
         }
 
-        private CloudBlockBlob GetBlockBlob(string storageName, string filePath)
+        private async Task<CloudBlockBlob> GetBlockBlobAsync(string storageName, string filePath)
         {
             CloudBlobContainer container = _blobClient.GetContainerReference(storageName);
+            await container.CreateIfNotExistsAsync();
+
             CloudBlockBlob blob = container.GetBlockBlobReference(filePath.ToLower());
 
             return blob;
