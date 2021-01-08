bc9d87d62b3d87dfb1b82bc637b24d3b6bf74a5c
Dijana Penić <penic.dijana@gmail.com>
2021-01-08 23:35:13 +0100

    Moved account verification endpoints into a separate controller - AccountVerifyController.

--- Store/Store.WebAPI/Controllers/Identity/RegisterController.cs
+++ Store/Store.WebAPI/Controllers/Identity/RegisterController.cs
@@ -102,83 +102,23 @@ namespace Store.WebAPI.Controllers
             // Save the registration cookie - if the cookie is present in the browser, the user can be automaticalled signed in. Otherwise, the user must be redirected to the Login page.
             await _signInManager.RegisterAsync(user);
 
-            // Send email confirmation token
-            await SendEmailConfirmationTokenAsync(clientId, user, registerUserModel.ActivationUrl);
+            // Get email confirmation token
+            string token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            _logger.LogInformation("Email confirmation token has been generated.");
+
+            UriTemplate template = new UriTemplate(registerUserModel.ActivationUrl);
+            string callbackUrl = template.Resolve(new Dictionary<string, object>
+            {
+                { "userId", user.Id.ToString() },
+                { "token", token.Base64ForUrlEncode() }
+            });
+
+            _logger.LogInformation("Sending account activation email to activate account.");
+
+            await _emailClientSender.SendConfirmAccountEmailAsync(clientId, user.Email, callbackUrl);
 
             return Ok();
-        }
-
-        /// <summary>Generates and sends the email confirmation token.</summary>
-        /// <param name="userId">The user identifier.</param>
-        /// <param name="clientId">The client identifier.</param>
-        /// <param name="returnUrl">The return URL.</param>
-        /// <returns>
-        ///   <br />
-        /// </returns>
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("{userId:guid}/verify/email")]
-        public async Task<IActionResult> SendEmailConfirmationTokenAsync([FromRoute] Guid userId, [FromQuery] Guid clientId, [FromQuery] string returnUrl)
-        {
-            if (userId == Guid.Empty)
-            {
-                return BadRequest("User Id cannot be empty.");
-            }
-
-            if (clientId == Guid.Empty)
-            {
-                return BadRequest("Client Id cannot be empty.");
-            }
-
-            if (string.IsNullOrWhiteSpace(returnUrl))
-            {
-                return BadRequest("Return URL cannot be empty.");
-            }
-
-            IUser user = await _userManager.FindUserByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound("User not found.");
-            }
-            if (user.EmailConfirmed)
-            {
-                return BadRequest("Email is already confirmed.");
-            }
-
-            await SendEmailConfirmationTokenAsync(clientId, user, returnUrl);
-
-            return Ok();
-        }
-
-        /// <summary>Confirms the user's email.</summary>
-        /// <param name="userId">The user identifier.</param>
-        /// <param name="token">The token.</param>
-        /// <returns>
-        ///   <br />
-        /// </returns>
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("{userId:guid}/verify/email/token/{token}")]
-        public async Task<IActionResult> ConfirmUserEmailAsync([FromRoute] Guid userId, [FromRoute] string token)
-        {
-            if (userId == Guid.Empty)
-            {
-                return BadRequest("User Id cannot be empty.");
-            }
-            if (string.IsNullOrWhiteSpace(token))
-            {
-                return BadRequest("Token is required.");
-            }
-
-            IUser user = await _userManager.FindByIdAsync(userId.ToString());
-            if (user == null)
-            {
-                NotFound();
-            }
-
-            IdentityResult result = await _userManager.ConfirmEmailAsync(user, token.Base64ForUrlDecode());
-
-            return result.Succeeded ? Ok() : BadRequest(result.Errors);
         }
 
         /// <summary>Either creates a new external login account or establishes the external provider association with the existing account (different email address).</summary>
@@ -325,174 +265,6 @@ namespace Store.WebAPI.Controllers
             _logger.LogInformation($"There is no user account registered with {registerModel.AssociateEmail} email.");
 
             return Ok(ExternalAuthStep.UserNotFound);
-        }
-
-        /// <summary>Confirms the external provider association by confirming the user's email.</summary>
-        /// <param name="userId">The user identifier.</param>
-        /// <param name="token">The token.</param>
-        /// <returns>
-        ///   <br />
-        /// </returns>
-        [HttpPost] 
-        [AllowAnonymous]
-        [Route("external/{userId:guid}/verify/email/token/{token}")]
-        public async Task<IActionResult> ConfirmExternalProviderAsync([FromRoute] Guid userId, [FromRoute] string token)
-        {
-            if (userId == Guid.Empty)
-            {
-                return BadRequest("User Id cannot be empty.");
-            }
-            if (string.IsNullOrWhiteSpace(token))
-            {
-                return BadRequest("Token is required.");
-            }
-
-            IUser user = await _userManager.FindByIdAsync(userId.ToString());
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            string decodedToken = token.Base64ForUrlDecode();
-
-            if (!user.EmailConfirmed)
-            {
-                // External provider is authenticated source so we can confirm the email
-                IdentityResult emailConfirmationResult = await _userManager.ConfirmEmailAsync(user, decodedToken);
-
-                if (!emailConfirmationResult.Succeeded) return BadRequest(emailConfirmationResult.Errors);
-            }
-
-            // Create a new external login for the user
-            IdentityResult confirmLoginResult = await _userManager.ConfirmLoginAsync(user, decodedToken);
-
-            if (!confirmLoginResult.Succeeded) return BadRequest(confirmLoginResult.Errors);
-
-            return Ok();
-        }
-
-        /// <summary>Generates the phone number confirmation token and sends it via SMS to the phone number.</summary>
-        /// <param name="userId">The user identifier.</param>
-        /// <param name="phoneNumberVerifyModel">The phone number verify model.</param>
-        /// <returns>
-        ///   <br />
-        /// </returns>
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("{userId:guid}/verify/sms")]
-        [Consumes("application/json")]
-        public async Task<IActionResult> SendPhoneNumberConfirmationTokenAsync([FromRoute] Guid userId, PhoneNumberVerifyPostApiModel phoneNumberVerifyModel)
-        {
-            if (userId == Guid.Empty)
-            {
-                return BadRequest("User Id cannot be empty.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            IUser user = await _userManager.FindUserByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound("User not found.");
-            }
-            if(user.PhoneNumberConfirmed)
-            {
-                return BadRequest("Phone number is already confirmed.");
-            }
-
-            // Retrieve countries lookup from cache or the database
-            IList<ICountry> countries = await _cacheProvider.GetOrAddAsync
-            (
-                CacheParameters.Keys.AllCountries,
-                () => _countriesService.GetCountriesAsync(),
-                DateTimeOffset.MaxValue,
-                CacheParameters.Groups.Settings
-            );
-
-            if(countries == null)
-            {
-                return InternalServerError();
-            }
-
-            ICountry country = countries.Where
-            (c => 
-                c.AlphaThreeCode == phoneNumberVerifyModel.IsoCountryCode && 
-                c.CallingCodes.Contains(phoneNumberVerifyModel.CountryCodeNumber.Trim('+'))
-            ).SingleOrDefault();
-
-            if (country == null)
-            {
-                return NotFound("Country not found.");
-            }
-
-            _logger.LogInformation("Generating phone number confirmation token.");
-
-            string phoneNumber = string.Concat(phoneNumberVerifyModel.CountryCodeNumber, phoneNumberVerifyModel.PhoneNumber);
-
-            // Get sms confirmation token
-            string token = await _userManager.GenerateChangePhoneNumberTokenAsync(user, phoneNumber.GetDigits());
-
-            _logger.LogInformation("Sending SMS confirmation token to activate the account.");
-
-            await _smsSender.SendSmsAsync(phoneNumber, $"Your Store verification code is: {token}.");
-
-            return Ok();
-        }
-
-        /// <summary>Confirms the phone number by checking the provided confirmation token.</summary>
-        /// <param name="userId">The user identifier.</param>
-        /// <param name="token">The token.</param>
-        /// <param name="phoneNumber">The phone number.</param>
-        /// <returns>
-        ///   <br />
-        /// </returns>
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("{userId:guid}/verify/sms/token/{token}")]
-        public async Task<IActionResult> ConfirmPhoneNumberAsync([FromRoute] Guid userId, [FromRoute] string token, [FromQuery] string phoneNumber)
-        {
-            if (userId == Guid.Empty)
-            {
-                return BadRequest("User Id cannot be empty.");
-            }
-
-            IUser user = await _userManager.FindUserByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            IdentityResult result = await _userManager.ChangePhoneNumberAsync(user, phoneNumber.GetDigits(), token);
-
-            return result.Succeeded ? Ok() : BadRequest(result.Errors);
-        }
-
-        private async Task SendEmailConfirmationTokenAsync(Guid clientId, IUser user, string activationUrl)
-        {
-            if (user == null)
-                throw new ArgumentNullException(nameof(user));
-
-            if (string.IsNullOrWhiteSpace(activationUrl))
-                throw new ArgumentNullException(nameof(activationUrl));
-
-            // Get email confirmation token
-            string token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-
-            _logger.LogInformation("Email confirmation token has been generated.");
-
-            UriTemplate template = new UriTemplate(activationUrl);
-            string callbackUrl = template.Resolve(new Dictionary<string, object>
-            {
-                { "userId", user.Id.ToString() },
-                { "token", token.Base64ForUrlEncode() }
-            });
-
-            _logger.LogInformation("Sending account activation email to activate account.");
-
-            await _emailClientSender.SendConfirmAccountEmailAsync(clientId, user.Email, callbackUrl);
         }
     }
 }
