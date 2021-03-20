ec3c56faeeb9e15b5a46e89e436a92b383848674
Dijana Penić <penic.dijana@gmail.com>
2021-03-20 13:13:18 +0100

    String extension method updates - cleanup and added a couple of new methods (ToPascalCase, FirstCharToUpper, GetCommaSeparated).

--- Store/Store.Common/Extensions/StringExtensions.cs
+++ Store/Store.Common/Extensions/StringExtensions.cs
@@ -2,11 +2,18 @@
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace Store.Common.Extensions
 {
     public static class StringExtensions
     {
+
+        public static string GetCommaSeparated(this string[] input)
+        {
+            return string.Join(',', input);
+        }
+
         public static string GetDigits(this string input)
         {
             return new string(input.Where(c => char.IsDigit(c)).ToArray());
@@ -16,9 +23,16 @@ namespace Store.Common.Extensions
         {
             if (string.IsNullOrEmpty(input)) { return input; }
 
-            var startUnderscores = Regex.Match(input, @"^_+");
+            Match startUnderscores = Regex.Match(input, @"^_+");
 
             return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
+
+        public static string ToPascalCase(this string input)
+        {
+            input = input.ToLower().Replace("_", string.Empty);
+            
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input);
         }
 
         public static string Base64ForUrlEncode(this string value)
@@ -33,6 +47,16 @@ namespace Store.Common.Extensions
             byte[] decodedBytes = Convert.FromBase64String(value);
 
             return Encoding.UTF8.GetString(decodedBytes);
+        }
+
+        public static string FirstCharToUpper(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+
+            return value.First().ToString().ToUpper() + value[1..];
         }
     }
 }
