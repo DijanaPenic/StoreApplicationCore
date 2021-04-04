4606e2eac22c43111a61976177b1a244cb53b6d3
Dijana Penić <penic.dijana@gmail.com>
2021-04-04 20:09:00 +0200

    Removed IsDescendingSortOrder const from the DefaultParameters class.

--- Store/Store.WebAPI/Constants/DefaultParameters.cs
+++ Store/Store.WebAPI/Constants/DefaultParameters.cs
@@ -5,7 +5,6 @@
         public const int PageSize = 3;
         public const int PageNumber = 1;
         public const string SearchString = null;
-        public const bool IsDescendingSortOrder = true;
         public const string SortOrder = "DateCreatedUtc|desc";
         public const string IncludeProperties = null;
     }
