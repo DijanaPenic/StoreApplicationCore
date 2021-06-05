230a77454c60a9466d3197049c415a9a94c4755d
Dijana Penić <penic.dijana@gmail.com>
2021-06-05 16:50:18 +0200

    HangfireExtensions class - decreased number of workers for Hangfire to reduce connections to database (Hangfire was creating 20+ connections simultaneously).

--- Store/Store.WebAPI/Startup/Extensions/HangfireExtensions.cs
+++ Store/Store.WebAPI/Startup/Extensions/HangfireExtensions.cs
@@ -14,21 +14,25 @@ namespace Store.WebAPI.Application.Startup.Extensions
         public static void AddHangfire(this IApplicationBuilder app)
         {
             // Configure hangfire dashboard and server
-            var tokenValidationParameters = (TokenValidationParameters)app.ApplicationServices.GetService(typeof(TokenValidationParameters));
-            var logger = (ILogger<HangfireDashboardAuthorizationAttribute>)app.ApplicationServices.GetService(typeof(ILogger<HangfireDashboardAuthorizationAttribute>));
+            TokenValidationParameters tokenValidationParameters = (TokenValidationParameters)app.ApplicationServices.GetService(typeof(TokenValidationParameters));
+            ILogger<HangfireDashboardAuthorizationAttribute> logger = (ILogger<HangfireDashboardAuthorizationAttribute>)app.ApplicationServices.GetService(typeof(ILogger<HangfireDashboardAuthorizationAttribute>));
 
             app.UseHangfireDashboard("/hangfire", new DashboardOptions
             {
                 Authorization = new[] { new HangfireDashboardAuthorizationAttribute(tokenValidationParameters, logger) }    // allow only admins to access the hangfire content
             });
-            app.UseHangfireServer();
+
+            BackgroundJobServerOptions serverOptions = new BackgroundJobServerOptions
+            {
+                WorkerCount = 1    //Hangfire's default worker count is 20, which opens 20 connections simultaneously. WorkerCount = 1 will open 6 connections.            
+            };
+            app.UseHangfireServer(serverOptions);
 
             // Configure hangfire daily job - recurring job at 9.00 AM Daily       
-            using (IServiceScope scope = app.ApplicationServices.CreateScope())
-            {
-                var authManager = (ApplicationAuthManager)scope.ServiceProvider.GetService(typeof(ApplicationAuthManager)); // ApplicationUserManager input parameter is a scoped service
-                RecurringJob.AddOrUpdate(() => authManager.RemoveExpiredRefreshTokensAsync(), "0 9 * * *");
-            }
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            ApplicationAuthManager authManager = (ApplicationAuthManager)scope.ServiceProvider.GetService(typeof(ApplicationAuthManager)); // ApplicationUserManager input parameter is a scoped service
+            RecurringJob.AddOrUpdate(() => authManager.RemoveExpiredRefreshTokensAsync(), "0 9 * * *");
         }
     }
 }
