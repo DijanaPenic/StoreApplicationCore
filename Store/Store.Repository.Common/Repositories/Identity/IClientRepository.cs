ee78c87102b33933471cf2dbce44ea8569bc754b
Dijana Penić <penic.dijana@gmail.com>
2021-06-10 16:59:45 +0200

    Repository refactoring (migrating methods to EF ORM) - Client.

--- Store/Store.Repository.Common/Repositories/Identity/IClientRepository.cs
+++ Store/Store.Repository.Common/Repositories/Identity/IClientRepository.cs
@@ -2,15 +2,17 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
+using Store.Common.Parameters.Sorting;
+using Store.Common.Parameters.Options;
 using Store.Model.Common.Models.Identity;
 
 namespace Store.Repository.Common.Repositories.Identity
 {
     public interface IClientRepository
     {
-        Task<IEnumerable<IClient>> GetAsync();
+        Task<IEnumerable<IClient>> GetAsync(ISortingParameters sorting, IOptionsParameters options = null);
 
-        Task<IClient> FindByKeyAsync(Guid key);
+        Task<IClient> FindByKeyAsync(Guid key, IOptionsParameters options = null);
 
         Task<IClient> FindByNameAsync(string name);
     }
--- Store/Store.Repository/Repositories/Identity/ClientRepository.cs
+++ Store/Store.Repository/Repositories/Identity/ClientRepository.cs
@@ -2,43 +2,41 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 using Store.DAL.Context;
-using Store.DAL.Schema.Identity;
-using Store.Models.Identity;
 using Store.Model.Common.Models.Identity;
 using Store.Repository.Core;
 using Store.Repository.Common.Repositories.Identity;
+using Store.Entities.Identity;
+using Store.Common.Parameters.Sorting;
+using Store.Common.Parameters.Options;
 
 namespace Store.Repositories.Identity
 {
     internal class ClientRepository : GenericRepository, IClientRepository
     {
+        private DbSet<ClientEntity> _dbSet => DbContext.Set<ClientEntity>();
+
         public ClientRepository(ApplicationDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
         { 
         }
 
-        public async Task<IEnumerable<IClient>> GetAsync()
+        public Task<IEnumerable<IClient>> GetAsync(ISortingParameters sorting, IOptionsParameters options = null)
         {
-            return await QueryAsync<Client>(
-                sql: $"SELECT * FROM {ClientSchema.Table}"
-            );
+            return GetAsync<IClient, ClientEntity>(sorting, options);
         }
 
-        public async Task<IClient> FindByKeyAsync(Guid key)
+        public Task<IClient> FindByKeyAsync(Guid key, IOptionsParameters options = null)
         {
-            return await QuerySingleOrDefaultAsync<Client>(
-                sql: $"SELECT * FROM {ClientSchema.Table} WHERE {ClientSchema.Columns.Id} = @{nameof(key)}",
-                param: new { key }
-            );
+            return FindByKeyAsync<IClient, ClientEntity>(options, key);
         }
 
         public async Task<IClient> FindByNameAsync(string name)
         {
-            return await QuerySingleOrDefaultAsync<Client>(
-                sql: $"SELECT * FROM {ClientSchema.Table} WHERE {ClientSchema.Columns.Name} = @{nameof(name)}",
-                param: new { name }
-            );
+            ClientEntity entity = await _dbSet.FirstOrDefaultAsync(c => c.Name == name);
+
+            return Mapper.Map<IClient>(entity);
         }
     }
 }
