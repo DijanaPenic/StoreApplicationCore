6159f6eedece441303b66a7d07391ab36ecaeca8
Dijana Penić <penic.dijana@gmail.com>
2021-07-02 15:39:41 +0200

    GuidHelper - code cleanup.

--- Store/Store.Common/Helpers/GuidHelper.cs
+++ Store/Store.Common/Helpers/GuidHelper.cs
@@ -7,18 +7,13 @@ namespace Store.Common.Helpers
     {
         private static SequentialGuidValueGenerator _sequentialGuidGenerator;
 
-        public static SequentialGuidValueGenerator SequentialGuidGenerator
+        private static SequentialGuidValueGenerator SequentialGuidGenerator
         {
             get
             {
                 _sequentialGuidGenerator ??= new SequentialGuidValueGenerator();
 
                 return _sequentialGuidGenerator;
-            }
-
-            set
-            {
-                _sequentialGuidGenerator = value;
             }
         }
 
@@ -27,13 +22,6 @@ namespace Store.Common.Helpers
         public static bool IsNullOrEmpty(object value)
         {
             return ((value as Guid?).GetValueOrDefault() == Guid.Empty);
-        }
-
-        public static Guid Get(string value)
-        {
-            Guid.TryParse(value, out Guid result);
-
-            return result;
         }
     }
 }
