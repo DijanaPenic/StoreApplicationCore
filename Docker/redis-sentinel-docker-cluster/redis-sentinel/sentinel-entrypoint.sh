ba7711504342d739182a56c11cd813949752e1fb
Dijana Penić <penic.dijana@gmail.com>
2021-07-08 14:29:13 +0200

    Redis Sentinel docker configuration - cleanup and added comments.

--- Docker/redis-sentinel-docker-cluster/redis-sentinel/sentinel-entrypoint.sh
+++ Docker/redis-sentinel-docker-cluster/redis-sentinel/sentinel-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
  
+# Replacing text in sentinel.conf file
+# /g to replace all the patterns from the nth occurrence of a pattern in a line
 sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /redis/sentinel.conf
- 
-redis-server /redis/sentinel.conf --sentinel
+
+# Run Sentinel 
+redis-server ./sentinel.conf --sentinel
